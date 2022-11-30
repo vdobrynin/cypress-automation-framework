5f2e91204a4b07682527e86582d2545c43b0d2b8
vdobrynin <vdobrynin@sbcglobal.net>
2022-11-29 19:02:15 -0800

    1st

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   integration: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here :)
     },
   },
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here :)
     },
   },
 });
