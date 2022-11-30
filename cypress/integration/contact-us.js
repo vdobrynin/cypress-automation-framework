4afd3428cfbc52968879c4f1579e4bb0102b2052
vdobrynin <vdobrynin@sbcglobal.net>
2022-11-29 19:01:37 -0800

    1st

---
+++ cypress/integration/contact-us.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe("Test Contact Us form via WebDriverUni", () =>  {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        //cypress code
+        
+    });
+
+    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+        //cypress code
+    });
+})
